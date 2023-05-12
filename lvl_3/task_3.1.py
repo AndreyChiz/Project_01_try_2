f1364fdf1de7212f51ef21de84166d3153ad665f
Andrey Chiz <AChzhovjub@yandex.ru>
2023-05-12 10:03:02 +0400

    task_3.1 done

--- lvl_3/task_3.1.py
+++ lvl_3/task_3.1.py
@@ -7,6 +7,8 @@
 #       * принимать новые значения, 
 #       * заменять существующие значения, 
 #       * выводить число строк и колонок.
+import random
+
 
 # Пример матрицы 10 на 10 из единиц:
 # [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -26,21 +28,45 @@
 #   - проявите фантазию :)
 
 class Matrix:
-    def __init__(self, rows: int = 3, colomn: int = 3, value: int | None = None):
+    '''
+    Класс матрица
+    '''
+    def __init__(self, *, rows: int = 3, colomns: int = 3, value: int | None = None):
+        '''
+        Создает двумерный список размерностью rows-строк на colomns-столбцов, заполняет занчениями value
+        :param rows: количество строк
+        :param colomns: количество столбцов
+        :param value: значение ячеек
+        '''
         self.rows = rows
-        self.colomns = colomn
+        self.colomns = colomns
         self.value = value
-        self.data = [[value] * self.colomns for i in range(rows)]
+        self.data = [[value] * self.colomns for i in range(self.rows)]
 
-    def __str__(self):
+    def __str__(self) -> str:
         result = ''
         for row in self.data:
             result += ' '.join([str(elem) for elem in row]) + '\n'
         return result
 
-    def add_element(self, *, x_coord: int, y_coord: int, value: int) -> None:
+    def change_all_values(self, new_value: int):
+        '''
+        Изменяет все значения матрицы на new_value
+        :param new_value:
+        :return:
+        '''
+        self.data = [[new_value] * self.colomns for i in range(self.rows)]
 
-        self.data[self.rows - y_coord][self.colomns - (self.colomns - x_coord) - 1] = value
+
+    def change_element(self, *, colomn: int, row: int, value: int) -> None:
+        '''
+        Заменяет элемент на указанной позиции, позиция[0][0] - лево, верх
+        :param colomn: номер столбца
+        :param row: номер ряда
+        :param value: значение
+        :return:
+        '''
+        self.data[row][colomn] = value
 
     def get_measure(self) -> tuple:
         '''
@@ -49,9 +75,28 @@ class Matrix:
         '''
         return self.rows, self.colomns
 
+    def rundomize_values(self,):
+        self.data = [[random.randint(0,9) for _ in range(self.colomns)] for i in range(self.rows)]
 
-matrix = Matrix(4, 8, 1)
-matrix.add_element(y_coord=1, x_coord=3, value=5)
 
-print(matrix)
-print(matrix.get_measure())
+if __name__ == '__main__':
+
+    #   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
+    #   - в каждой ячейке содержится либо число, либо None
+    matrix = Matrix(rows=4, colomns=8, value=1)
+    print(matrix)
+
+    # выводить число строк и колонок
+    measure = matrix.get_measure()
+    print(f'Размер матрицы:\nкол-во строк - {measure[0]}\nкол-во колонок - {measure[1]}\n')
+
+    # заменять существующие значения
+    matrix.change_element(row=1, colomn=3, value=5)
+    print(matrix)
+
+    # принимать новые значения,
+    matrix.change_all_values(8)
+    print(matrix)
+
+    matrix.rundomize_values()
+    print(matrix)
